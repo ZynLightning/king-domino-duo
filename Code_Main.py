435e947746d3c72833610b65c0743c11e514950e
SimonSeaAxe <zimongarner@gmail.com>
2024-09-12 15:42:25 +0200

    Tilføjede mine

--- Code_Main.py
+++ Code_Main.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-BGRimage = cv.imread("1.jpg")
+BGRimage = cv.imread("2.jpg")
 HSVimage = cv.cvtColor(BGRimage,cv.COLOR_BGR2HSV)
 board=np.zeros((5,5), dtype=str)
 for i in range(5):
@@ -11,6 +11,7 @@ for i in range(5):
         forestValue=0
         fieldValue=0
         wasteValue=0
+        mineValue=0
         for row in HSVimage[100*i:100*(i+1),100*j:100*(j+1)]:
             for pixel in row:
                 if 102<pixel[0]<111: #check if the pixel is typical for ocean
@@ -19,20 +20,26 @@ for i in range(5):
                     grassLandValue+=1
                 elif (49/255*180)<pixel[0]<(87/255*180) and pixel[1]<210 and pixel[2]<85:
                     forestValue+=1
-                elif (26/255*180)<pixel[0]<(41/255*180) and 223<pixel[1]<255 and 191<pixel[2]:
+                elif (26/255*180)<pixel[0]<(41/255*180) and 223<pixel[1]<255 and 168<pixel[2]:
                     fieldValue+=1
                 elif (16/255*180)<pixel[0]<(34/255*180) and pixel[1]<172 and 51<pixel[2]<151:
                     wasteValue+=1
-        if oceanValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue]):
+                elif pixel[0]<(44/255*180) and pixel[1]<113 and pixel[2]<79:
+                    mineValue+=1
+        if 1000>np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue,mineValue]):
+            board[i,j]="Ki"        
+        elif oceanValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue,mineValue]):
             board[i,j]="Oc"
-        elif grassLandValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue]):
+        elif grassLandValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue,mineValue]):
             board[i,j]="Gr"
-        elif forestValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue]):
+        elif forestValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue,mineValue]):
             board[i,j]="Tr"
-        elif fieldValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue]):
+        elif fieldValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue,mineValue]):
             board[i,j]="Fi"
-        elif wasteValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue]):
+        elif wasteValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue,mineValue]):
             board[i,j]="Wa"
+        elif mineValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue,mineValue]):
+            board[i,j]="Mi"
         
-        #print(f"[{j+1},{i+1}] oceanVal={oceanValue}, grassVal={grassLandValue}, forestVal={forestValue}, fieldVal={fieldValue}, wasteVal={wasteValue}")
+        print(f"[{j+1},{i+1}] oceanVal={oceanValue}, grassVal={grassLandValue}, forestVal={forestValue}, fieldVal={fieldValue}, wasteVal={wasteValue}, mineVal={mineValue}")
 print(board)
