97609e777f0cb8497fd80960f4bd9b07be0fb3e2
SimonSeaAxe <zimongarner@gmail.com>
2024-09-12 15:00:31 +0200

    Update Code_Main.py

--- Code_Main.py
+++ Code_Main.py
@@ -3,12 +3,36 @@ import numpy as np
 
 BGRimage = cv.imread("1.jpg")
 HSVimage = cv.cvtColor(BGRimage,cv.COLOR_BGR2HSV)
+board=np.zeros((5,5), dtype=str)
 for i in range(5):
     for j in range(5):
         oceanValue=0
+        grassLandValue=0
+        forestValue=0
+        fieldValue=0
+        wasteValue=0
         for row in HSVimage[100*i:100*(i+1),100*j:100*(j+1)]:
             for pixel in row:
-                if 102<pixel[0]<111:
+                if 102<pixel[0]<111: #check if the pixel is typical for ocean
                     oceanValue+=1
-                
-        print(oceanValue)
+                elif (53/255*180)<pixel[0]<(79/255*180) and 130<pixel[2]<255:
+                    grassLandValue+=1
+                elif (49/255*180)<pixel[0]<(87/255*180) and pixel[1]<210 and pixel[2]<85:
+                    forestValue+=1
+                elif (26/255*180)<pixel[0]<(41/255*180) and 223<pixel[1]<255 and 191<pixel[2]:
+                    fieldValue+=1
+                elif (16/255*180)<pixel[0]<(34/255*180) and pixel[1]<172 and 51<pixel[2]<151:
+                    wasteValue+=1
+        if oceanValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue]):
+            board[i,j]="Oc"
+        elif grassLandValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue]):
+            board[i,j]="Gr"
+        elif forestValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue]):
+            board[i,j]="Tr"
+        elif fieldValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue]):
+            board[i,j]="Fi"
+        elif wasteValue==np.max([oceanValue,grassLandValue,forestValue,fieldValue,wasteValue]):
+            board[i,j]="Wa"
+        
+        #print(f"[{j+1},{i+1}] oceanVal={oceanValue}, grassVal={grassLandValue}, forestVal={forestValue}, fieldVal={fieldValue}, wasteVal={wasteValue}")
+print(board)
