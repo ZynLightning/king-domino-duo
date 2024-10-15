3fdbc536fad9bfe47cbff0bdceded605216c980b
SimonSeaAxe <zimongarner@gmail.com>
2024-10-15 09:47:54 +0200

    Changed Main

--- mainCode.py
+++ mainCode.py
@@ -1,10 +1,17 @@
+import cv2 as cv
 from findBlobs import blobs, biomes
-
+from templateSearch import Template_Searcher
 biome = biomes()
 blob=blobs()
+ts = Template_Searcher()
+
 
 boardNumber="1"#input("Which board do you want counted?")
 biome.findBiomes(f"boards/{boardNumber}.jpg")
 board=biome.findBoard()
-bloblist=blob.wildfire()
-print(bloblist)
+crownList=ts.program(cv.imread(f"boards/{boardNumber}.jpg"),board)
+blobList=blob.wildfire()
+
+print(crownList)
+
+print(blobList)
